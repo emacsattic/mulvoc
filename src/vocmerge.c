8761239901fbee08dd2f64c27aa949cc8420c537
jcg_sturdy <jcg_sturdy>
2009-06-26 15:17:32 +0000

    new formats, mostly

--- src/vocmerge.c
+++ src/vocmerge.c
@@ -1,5 +1,5 @@
 /* vocmerge.c
-   Time-stamp: <2009-05-15 22:05:43 jcgs>
+   Time-stamp: <2009-06-24 09:28:42 jcgs>
    Vocabulary file merge program in the MuLVoc suite.
  */
 
@@ -9,24 +9,35 @@
 #include <string.h>
 #include "../libsrc/mulvoc.h"
 
+#if 1
+#define ENABLE_TRACE_MERGE 1
+#endif
+
 #define START_SIZE 24
 
 #define BUFFER_SIZE (256*256)
 
-static const char *short_options = "a:b:cHhl:o:prs:t:v";
+static const char *short_options = "a:b:cd:E:gHhl:mo:prS:s:t:v";
 
 static struct option long_options[] = {
   {"attributes", required_argument, 0, 'a'},
   {"blank", required_argument, 0, 'b'},
   {"controlled", no_argument, 0, 'c'},
+  {"delimited", required_argument, 0, 'd'},
   {"help", no_argument, 0, 'H'},
   {"html", no_argument, 0, 'h'},
   {"html-page", no_argument, 0, 'p'},
   {"languages", required_argument, 0, 'l'},
+  {"locate", no_argument, 0, 'g'},
   {"output", required_argument, 0, 'o'},
   {"raw", no_argument, 0, 'r'},
+  {"row-end", required_argument, 0, 'E'},
+  {"row-start", required_argument, 0, 'S'},
   {"sort", required_argument, 0, 's'},
   {"title", required_argument, 0, 't'},
+#ifdef ENABLE_TRACE_MERGE
+  {"trace-merge", no_argument, 0, 'm'},
+#endif
   {"verbose", no_argument, 0, 'v'},
   {0, 0, 0, 0}
 };
@@ -36,7 +47,7 @@ print_usage(char *progname)
 {
   fprintf(stderr, "Usage: %s [options] file ... file\n", progname);
   fprintf(stderr, "\nOptions:\n");
-  fprintf(stderr, "  --languages A,B,C,D      output columns A B C D, in that order\n");
+  fprintf(stderr, "  --languages (-l) A,B,C   output columns A B C, in that order\n");
   fprintf(stderr, "  --sort (-s) LANGUAGE     sort output by LANGUAGE\n");
   fprintf(stderr, "  --output (-o) FILE       output to FILE (otherwise stdout)\n");
   fprintf(stderr, "  --html (-h)              output bare html table code\n");
@@ -45,11 +56,18 @@ print_usage(char *progname)
   fprintf(stderr, "  --attributes (-a) ATTRS  use ATTRS for html table attributes\n");
   fprintf(stderr, "  --blank (-b) TEXT        use TEXT for blank cells\n");
   fprintf(stderr, "  --controlled (-c)        use colours etc from input\n");
-  fprintf(stderr, "  --verbose (-b)           turn on debugging\n");
+  fprintf(stderr, "  --locate (-g)            show filenames and line numbers\n");
+  fprintf(stderr, "  --delimited (-d) DELIM   output plain text with DELIM between cells\n");
+  fprintf(stderr, "  --row-start (-S) STRING  use STRING at start of each line with -d\n");
+  fprintf(stderr, "  --row-end (-S) STRING    use STRING at end of each line with -d\n");
+  fprintf(stderr, "  --verbose (-v)           turn on debugging\n");
+#ifdef ENABLE_TRACE_MERGE
+  fprintf(stderr, "  --trace-merge (-m)       turn on merge debugging\n");
+#endif
   fprintf(stderr, "\n--title implies --html-page\n");
   fprintf(stderr, "--attributes or --blank or --controlled imply --html\n");
   fprintf(stderr, "--output whatever.htm[l] implies --html\n");
-  fprintf(stderr, "\nIf html is not selected, output is as csv.\n");
+  fprintf(stderr, "\nIf no other format is selected, output is as csv.\n");
 }
 
 int
@@ -65,8 +83,16 @@ main(int argc, char **argv)
   FILE *output_stream = stdout;
   char *language_code = "ENG";
   char *languages_string = NULL;
+  char *delimiter = NULL;
+  char *row_start = NULL;
+  char *row_end = "\n";
   int verbose = 0;
+#ifdef ENABLE_TRACE_MERGE
+  int trace_merge = 0;
+#endif
   int table_controlled = 0;
+  int show_locations = 0;
+  int files_read = 0;
 
   vocabulary_table table;
 
@@ -89,8 +115,12 @@ main(int argc, char **argv)
     case 'a': html = 1; attributes = optarg; break;
     case 'b': html = 1; blank = optarg; break;
     case 'c': table_controlled = 1; html = 1; break;
+    case 'g': show_locations = 1; break;
     case 'h': html = 1; break;
     case 'l': languages_string = optarg; break;
+#ifdef ENABLE_TRACE_MERGE
+    case 'm': trace_merge = 1; break;
+#endif
     case 'o': output_file_name = optarg;
       if ((strcmp(output_file_name + strlen(output_file_name) - 5, ".html") == 0)
 	  || (strcmp(output_file_name + strlen(output_file_name) - 4, ".htm") == 0)) {
@@ -102,14 +132,33 @@ main(int argc, char **argv)
     case 's': language_code = optarg; break;
     case 't': page = html = 1; title = optarg; break;
     case 'v': verbose = 1; break;
+#ifndef ENABLE_TRACE_MERGE
+    case 'm':
+#endif
     default: print_usage(argv[0]); exit(1); break;
     }
   }
 
-  mulvoc_initialize_table(&table, 1511, START_SIZE, verbose ? -1 : 0);
+  mulvoc_initialize_table(&table, 1511, START_SIZE,
+			  ((verbose ? (TRACE_READ
+				       | TRACE_HEADERS
+				       | TRACE_PRAGMATA
+				       | TRACE_COMMENTS) : 0)
+			   | (show_locations ? TRACE_ORIGINS : 0)
+#ifdef ENABLE_TRACE_MERGE
+			   | (trace_merge ? TRACE_MERGE : 0)
+#endif
+			   ));
 
   for (; optind < argc; optind++) {
     read_vocab_file(argv[optind], &table);
+    files_read++;
+  }
+
+  if (files_read == 1) {
+    /* If only one file, print only line numbers, and not filenames,
+       for locations */
+    table.location_format = "%2$d";
   }
 
   if (output_file_name != NULL) {
@@ -117,11 +166,24 @@ main(int argc, char **argv)
   }
 
   {
-    int key_idx = language_index(&table, language_code, strlen(language_code));
+    int key_idx = language_index(&table,
+				 language_code,
+				 strlen(language_code));
     int *languages = NULL;
-    int n_languages = language_indices(&table, languages_string, &languages);
+    int n_languages = language_indices(&table,
+				       languages_string,
+				       &languages);
 
-    if (raw) {
+    if (delimiter != NULL) {
+      mulvoc_output_delimited(output_stream,
+			      &table,
+			      languages,
+			      n_languages,
+			      key_idx,
+			      row_start,
+			      delimiter,
+			      row_end);
+    } else if (raw) {
       mulvoc_output_data(output_stream,
 			 &table,
 			 languages, n_languages,
